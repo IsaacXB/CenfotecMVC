5d4b9fcd03fae7387b04efc9685f2a35805cb7e6
IsaacXB <isachatr@outlook.com>
2022-08-09 12:45:29 -0600

    Práctica Servicios #1

--- AppCenfoMusica/AppCenfoMusica.Servicios/Controllers/ServiceController.cs
+++ AppCenfoMusica/AppCenfoMusica.Servicios/Controllers/ServiceController.cs
@@ -89,7 +89,169 @@ namespace AppCenfoMusica.Servicios.Controllers
         }
         #endregion
 
-#region Cliente
-#endregion
+        #region Clientes
+
+        //Get: api/Service/ListaClientes
+        [HttpGet("ListaClientes")]
+        public IEnumerable<BaseDTO> ListaClientes()
+        {
+            var resultado = new ClienteLogica().ListaClientes();
+
+            if (resultado.ElementAt(0).GetType() != typeof(ErrorDTO))
+            {
+                return resultado.OfType<ClienteDTO>();
+            }
+            else
+            {
+                return resultado.OfType<ErrorDTO>();
+            }
+        }
+
+        //Get: api/Service/BuscarClientePorId/{id}
+        [HttpGet("BuscarClientePorId/{id}")]
+        public BaseDTO BuscarClientePorId(int id)
+        {
+            var resultado = new ClienteLogica().BuscarClientePorID(id);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (ClienteDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+
+        //Get: api/Service/AgregarCliente/
+        [HttpPost("AgregarCliente")]
+        public BaseDTO AgregarCliente(ClienteDTO cliente)
+        {
+            var resultado = new ClienteLogica().AgregarCliente(cliente);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (ClienteDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+        //Get: api/Service/AgregarClienteParametros/
+        [HttpPost("AgregarClienteParametros")]
+        public BaseDTO AgregarClienteParametros(string nombre, string email, string nomUsuario, string contrasena,
+            string cedula, int estado, int sexo, DateTime fechaNacimiento, string telefono)
+        {
+            var resultado = new ClienteLogica().AgregarCliente(nombre, email, nomUsuario, contrasena, cedula, estado, sexo, fechaNacimiento, telefono);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (ClienteDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+
+        [HttpPost("ActualizarCliente")]
+        public BaseDTO ActualizarPrecioProducto(int codigoCliente,string nombre, string email, string nomUsuario, string contrasena,
+            string cedula, int estado, int sexo, DateTime fechaNacimiento, string telefono)
+        {
+            var resultado = new ClienteLogica().ActualizarCliente(codigoCliente,nombre, email, nomUsuario, contrasena, cedula, estado, sexo, fechaNacimiento, telefono);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (ClienteDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+        #endregion
+
+        #region Vendedores
+
+        //Get: api/Service/ListaVendedores
+        [HttpGet("ListaVendedores")]
+        public IEnumerable<BaseDTO> ListaVendedores()
+        {
+            var resultado = new VendedorLogica().ListaVendedores();
+
+            if (resultado.ElementAt(0).GetType() != typeof(ErrorDTO))
+            {
+                return resultado.OfType<VendedorDTO>();
+            }
+            else
+            {
+                return resultado.OfType<ErrorDTO>();
+            }
+        }
+
+        //Get: api/Service/BuscarVendedorPorId/{id}
+        [HttpGet("BuscarVendedorPorId/{id}")]
+        public BaseDTO BuscarVendedorPorId(int id)
+        {
+            var resultado = new VendedorLogica().BuscarVendedorPorID(id);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (VendedorDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+
+        //Get: api/Service/AgregarVendedor/
+        [HttpPost("AgregarVendedor")]
+        public BaseDTO AgregarVendedor(VendedorDTO vendedor)
+        {
+            var resultado = new VendedorLogica().AgregarVendedor(vendedor);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (VendedorDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+        //Get: api/Service/AgregarVendedorParametros/
+        [HttpPost("AgregarVendedorParametros")]
+        public BaseDTO AgregarVendedorParametros(string nombre, int puesto, string nomUsuario, string contrasena, string cedula, int estado)
+        {
+            var resultado = new VendedorLogica().AgregarVendedor(nombre, puesto, nomUsuario, contrasena, cedula, estado);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (VendedorDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+
+        [HttpPost("ActualizarVendedor")]
+        public BaseDTO ActualizarVendedor(int codigoVendedor, string nombre, int puesto, string nomUsuario, string contrasena, string cedula, int estado)
+        {
+            var resultado = new VendedorLogica().ActualizarVendedor(codigoVendedor, nombre, puesto, nomUsuario, contrasena, cedula, estado);
+
+            if (resultado.GetType() != typeof(ErrorDTO))
+            {
+                return (VendedorDTO)resultado;
+            }
+            else
+            {
+                return (ErrorDTO)resultado;
+            }
+        }
+        #endregion
+
     }
 }
